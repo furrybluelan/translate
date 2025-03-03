d8676474e614cb46b99e6d147dc60922f457a9a3
furrybluelan <supollad@proton.me>
2025-03-03 16:54:05 +0800

    Add files via upload

--- output.py
+++ output.py
@@ -1,1 +1,9 @@
-from rich import *
+from rich import console
+from rich import text
+from rich import panel
+from rich import table
+import asyncio
+con = console.Console
+class output():
+    def __init__(another):
+        pass
