8d9737fad4a9ff47cd3d315d82e3dd38f22fa446
furrybluelan <supollad@proton.me>
2025-03-17 16:54:30 +0800

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -1,4 +1,18 @@
 import sys
-import argparse
 import asyncio
 import aiofile
+import fileinput
+from rich.console import Console
+
+
+console = Console()
+
+class GetInput():
+  def __init__(self):
+    self.main()
+  async def _stdin():
+    return fileinput.input()
+  async def _input():
+    return console.input("[cyan]请输入翻译内容：[/cyan]")
+  async def _file():
+    args.file
